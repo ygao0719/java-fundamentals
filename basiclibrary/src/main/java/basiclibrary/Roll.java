f57009f1dcda619d594c48c295281dd4c6f4ed29
Yuan Gao <example@email.com>
2019-05-29 09:34:00 -0700

    update before new branch

---
+++ basiclibrary/src/main/java/basiclibrary/Roll.java
@@ -0,0 +1,12 @@
+package basiclibrary;
+
+public class Roll {
+    public int[] roll(int n){
+        int[] rollResult = new int[n];
+        for (int i = 0; i < n; i ++){
+            rollResult[i] = (int)(Math.random() * ((6 - 1) + 1)) + 1;
+        }
+
+        return rollResult;
+    }
+}
---
+++ basiclibrary/src/test/java/basiclibrary/RollTest.java
@@ -0,0 +1,12 @@
+package basiclibrary;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RollTest {
+    @Test
+    public void testRoll(){
+        Roll libraryForTesting = new Roll();
+        assertEquals(message: "");
+    }
+}
