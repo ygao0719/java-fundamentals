ebf7fc8e69efbeecbe353a598f31aedc16be1393
Yuan Gao <example@email.com>
2019-06-04 22:14:02 -0700

    start class shop and class movies

---
+++ inheritance/src/main/java/inheritance/Shop.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class Shop {
+    private String name;
+    private String descrip;
+    private int numOfDollor;
+
+    public Shop(String name, String descrip, int numOfDollor){
+        this.name = name;
+        this.descrip = descrip;
+        this.numOfDollor = numOfDollor;
+    }
+
+    public String toSting(){
+        return String.format("");
+    }
+}
---
+++ inheritance/src/main/java/inheritance/Theater.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class Theater {
+    private String theaterName;
+    private String movies;
+
+    public Theater(String theaterName, String movies){
+
+        this.theaterName = theaterName;
+        this.movies = movies;
+    }
+}
